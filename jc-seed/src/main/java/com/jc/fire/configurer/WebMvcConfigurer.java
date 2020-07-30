dd6434e6a1b24a2e18e6316694bbf65f7c8ac053
DuanXin2565 <duanx_2565@163.com>
2020-07-30 09:03:47 +0800

    checkStyle

--- jc-seed/src/main/java/com/jc/fire/configurer/WebMvcConfigurer.java
+++ jc-seed/src/main/java/com/jc/fire/configurer/WebMvcConfigurer.java
@@ -45,15 +45,17 @@ import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 public class WebMvcConfigurer extends WebMvcConfigurerAdapter {
 
     private final Logger logger = LoggerFactory.getLogger(WebMvcConfigurer.class);
+    //当前激活的配置文件
     @Value("${spring.profiles.active}")
-    private String env;//当前激活的配置文件
+    private String env;
 
     //使用阿里 FastJson 作为JSON MessageConverter
     @Override
     public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
         FastJsonHttpMessageConverter converter = new FastJsonHttpMessageConverter();
         FastJsonConfig config = new FastJsonConfig();
-        config.setSerializerFeatures(SerializerFeature.WriteMapNullValue);//保留空的字段
+        //保留空的字段
+        config.setSerializerFeatures(SerializerFeature.WriteMapNullValue);
         //SerializerFeature.WriteNullStringAsEmpty,//String null -> ""
         //SerializerFeature.WriteNullNumberAsZero//Number null -> 0
         // 按需配置，更多参考FastJson文档哈
@@ -83,14 +85,18 @@ public class WebMvcConfigurer extends WebMvcConfigurerAdapter {
         exceptionResolvers.add(new HandlerExceptionResolver() {
             public ModelAndView resolveException(HttpServletRequest request, HttpServletResponse response, Object handler, Exception e) {
                 Result result = new Result();
-                if (e instanceof ServiceException) {//业务失败的异常，如“账号或密码错误”
+                //业务失败的异常，如“账号或密码错误”
+                if (e instanceof ServiceException) {
                     result.setCode(ResultCode.FAIL).setMessage(e.getMessage());
                     logger.info(e.getMessage());
-                } else if (e instanceof NoHandlerFoundException) {
+                }
+                else if (e instanceof NoHandlerFoundException) {
                     result.setCode(ResultCode.NOT_FOUND).setMessage("接口 [" + request.getRequestURI() + "] 不存在");
-                } else if (e instanceof ServletException) {
+                }
+                else if (e instanceof ServletException) {
                     result.setCode(ResultCode.FAIL).setMessage(e.getMessage());
-                } else {
+                }
+                else {
                     result.setCode(ResultCode.INTERNAL_SERVER_ERROR).setMessage("接口 [" + request.getRequestURI() + "] 内部错误，请联系管理员");
                     String message;
                     if (handler instanceof HandlerMethod) {
@@ -100,7 +106,8 @@ public class WebMvcConfigurer extends WebMvcConfigurerAdapter {
                                 handlerMethod.getBean().getClass().getName(),
                                 handlerMethod.getMethod().getName(),
                                 e.getMessage());
-                    } else {
+                    }
+                    else {
                         message = e.getMessage();
                     }
                     logger.error(message, e);
@@ -122,7 +129,8 @@ public class WebMvcConfigurer extends WebMvcConfigurerAdapter {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         //接口签名认证拦截器，该签名认证比较简单，实际项目中可以使用Json Web Token或其他更好的方式替代。
-        if (!"dev".equals(env)) { //开发环境忽略签名认证
+        //开发环境忽略签名认证
+        if (!"dev".equals(env)) {
             registry.addInterceptor(new HandlerInterceptorAdapter() {
                 @Override
                 public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
@@ -130,7 +138,8 @@ public class WebMvcConfigurer extends WebMvcConfigurerAdapter {
                     boolean pass = validateSign(request);
                     if (pass) {
                         return true;
-                    } else {
+                    }
+                    else {
                         logger.warn("签名认证失败，请求接口：{}，请求IP：{}，请求参数：{}",
                                 request.getRequestURI(), getIpAddress(request), JSON.toJSONString(request.getParameterMap()));
 
@@ -150,7 +159,8 @@ public class WebMvcConfigurer extends WebMvcConfigurerAdapter {
         response.setStatus(200);
         try {
             response.getWriter().write(JSON.toJSONString(result));
-        } catch (IOException ex) {
+        }
+        catch (IOException ex) {
             logger.error(ex.getMessage());
         }
     }
@@ -162,23 +172,29 @@ public class WebMvcConfigurer extends WebMvcConfigurerAdapter {
      * 3. 混合密钥（secret）进行md5获得签名，与请求的签名进行比较
      */
     private boolean validateSign(HttpServletRequest request) {
-        String requestSign = request.getParameter("sign");//获得请求签名，如sign=19e907700db7ad91318424a97c54ed57
+        //获得请求签名，如sign=19e907700db7ad91318424a97c54ed57
+        String requestSign = request.getParameter("sign");
         if (StringUtils.isEmpty(requestSign)) {
             return false;
         }
         List<String> keys = new ArrayList<String>(request.getParameterMap().keySet());
-        keys.remove("sign");//排除sign参数
-        Collections.sort(keys);//排序
+        //排除sign参数
+        keys.remove("sign");
+        //排序
+        Collections.sort(keys);
 
         StringBuilder sb = new StringBuilder();
         for (String key : keys) {
-            sb.append(key).append("=").append(request.getParameter(key)).append("&");//拼接字符串
+            //拼接字符串
+            sb.append(key).append("=").append(request.getParameter(key)).append("&");
         }
         String linkString = sb.toString();
-        linkString = StringUtils.substring(linkString, 0, linkString.length() - 1);//去除最后一个'&'
-
-        String secret = "Potato";//密钥，自己修改
-        String sign = DigestUtils.md5Hex(linkString + secret);//混合密钥md5
+        //去除最后一个'&'
+        linkString = StringUtils.substring(linkString, 0, linkString.length() - 1);
+        //密钥，自己修改
+        String secret = "Potato";
+        //混合密钥md5
+        String sign = DigestUtils.md5Hex(linkString + secret);
 
         return StringUtils.equals(sign, requestSign);//比较
     }
