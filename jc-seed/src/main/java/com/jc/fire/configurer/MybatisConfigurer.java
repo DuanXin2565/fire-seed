e92feadab367ffd694b85a8b7766e5bd8d59bf03
DuanXin2565 <duanx_2565@163.com>
2020-09-08 14:33:35 +0800

    新增多数据源配置

--- jc-seed/src/main/java/com/jc/fire/configurer/MybatisConfigurer.java
+++ jc-seed/src/main/java/com/jc/fire/configurer/MybatisConfigurer.java
@@ -3,15 +3,14 @@ package com.jc.fire.configurer;
 import java.util.Properties;
 import javax.sql.DataSource;
 
-import org.apache.ibatis.plugin.Interceptor;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.core.io.support.ResourcePatternResolver;
 
-import com.github.pagehelper.PageHelper;
 import com.jc.fire.core.ProjectConstant;
 import tk.mybatis.spring.mapper.MapperScannerConfigurer;
 
@@ -21,25 +20,39 @@ import tk.mybatis.spring.mapper.MapperScannerConfigurer;
 @Configuration
 public class MybatisConfigurer {
 
+    /**
+     * 使 application.properties配置生效，如果不主动配置，由于@Order配置顺序不同，
+     * 将导致配置不能及时生效 多数据源配置驼峰法生效
+     *
+     * @return 数据源
+     */
     @Bean
-    public SqlSessionFactory sqlSessionFactoryBean(DataSource dataSource) throws Exception {
+    @ConfigurationProperties(prefix = "mybatis.configuration")
+    public org.apache.ibatis.session.Configuration globalConfiguration() {
+
+        return new org.apache.ibatis.session.Configuration();
+
+    }
+
+    @Bean
+    public SqlSessionFactory sqlSessionFactoryBean(DataSource dataSource, org.apache.ibatis.session.Configuration config) throws Exception {
         SqlSessionFactoryBean factory = new SqlSessionFactoryBean();
         factory.setDataSource(dataSource);
         factory.setTypeAliasesPackage(ProjectConstant.MODEL_PACKAGE);
-
-        //配置分页插件，详情请查阅官方文档
-        PageHelper pageHelper = new PageHelper();
-        Properties properties = new Properties();
-        //分页尺寸为0时查询所有纪录不再执行分页
-        properties.setProperty("pageSizeZero", "true");
-        //页码<=0 查询第一页，页码>=总页数查询最后一页
-        properties.setProperty("reasonable", "true");
-        //支持通过 Mapper 接口参数来传递分页参数
-        properties.setProperty("supportMethodsArguments", "true");
-        pageHelper.setProperties(properties);
+        factory.setConfiguration(config);
+//        //配置分页插件，详情请查阅官方文档
+//        PageHelper pageHelper = new PageHelper();
+//        Properties properties = new Properties();
+//        //分页尺寸为0时查询所有纪录不再执行分页
+//        properties.setProperty("pageSizeZero", "true");
+//        //页码<=0 查询第一页，页码>=总页数查询最后一页
+//        properties.setProperty("reasonable", "true");
+//        //支持通过 Mapper 接口参数来传递分页参数
+//        properties.setProperty("supportMethodsArguments", "true");
+//        pageHelper.setProperties(properties);
 
         //添加插件
-        factory.setPlugins(new Interceptor[]{pageHelper});
+//        factory.setPlugins(new Interceptor[]{pageHelper});
 
         //添加XML目录
         ResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
